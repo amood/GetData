3fff002a03b87f7851f3426bd62775ec135807c5
amood <jamesrazor@163.com>
2021-01-01 11:52:15 +0800

    Add files via upload
    修改异常后提示错误

--- GetData_core.py
+++ GetData_core.py
@@ -61,6 +61,190 @@ def getfilename(filepath, json_data, conf: config.Config) -> str:
     return filename
 
 
+def get_data_from_json(file_number, filepath, conf: config.Config):  # 从JSON返回元数据
+    """
+    iterate through all services and fetch the data
+    """
+
+    func_mapping = {
+        "airav": airav.main,
+        "avsox": avsox.main,
+        "fc2": fc2.main,
+        "fanza": fanza.main,
+        "javdb": javdb.main,
+        "javbus": javbus.main,
+        "mgstage": mgstage.main,
+        "jav321": jav321.main,
+        "xcity": xcity.main,
+        "javlib": javlib.main,
+        "dlsite": dlsite.main,
+    }
+
+    # default fetch order list, from the beginning to the end
+    sources = conf.sources().split(',')
+
+    # if the input file name matches certain rules,
+    # move some web service to the beginning of the list
+    if "avsox" in sources and (re.match(r"^\d{5,}", file_number) or
+                               "HEYZO" in file_number or "heyzo" in file_number or "Heyzo" in file_number
+    ):
+        # if conf.debug() == True:
+        #     print('[+]select avsox')
+        sources.insert(0, sources.pop(sources.index("avsox")))
+    elif "mgstage" in sources and (re.match(r"\d+\D+", file_number) or
+                                   "siro" in file_number or "SIRO" in file_number or "Siro" in file_number
+    ):
+        # if conf.debug() == True:
+        # print('[+]select fanza')
+        sources.insert(0, sources.pop(sources.index("mgstage")))
+    elif "fc2" in sources and ("fc2" in file_number or "FC2" in file_number
+    ):
+        # if conf.debug() == True:
+        #     print('[+]select fc2')
+        sources.insert(0, sources.pop(sources.index("fc2")))
+    elif "dlsite" in sources and (
+            "RJ" in file_number or "rj" in file_number or "VJ" in file_number or "vj" in file_number
+    ):
+        # if conf.debug() == True:
+        #     print('[+]select dlsite')
+        sources.insert(0, sources.pop(sources.index("dlsite")))
+
+    json_data = {}
+    for source in sources:
+        try:
+            if conf.debug() == True:
+                print('[+]select', source)
+            json_data = json.loads(func_mapping[source](file_number))
+            # if any service return a valid return, break
+            if get_data_state(json_data):
+                break
+        except:
+            break
+
+    # Return if data not found in all sources
+    if not json_data:
+        print('[-]Movie Data not found!')
+        # moveFailedFolder(filepath, conf.failed_folder()) 注释掉的内容
+        return
+
+    # ================================================网站规则添加结束================================================
+
+    title = json_data.get('title')
+    actor_list = str(json_data.get('actor')).strip("[ ]").replace("'", '').split(',')  # 字符串转列表
+    release = json_data.get('release')
+    number = json_data.get('number')
+    studio = json_data.get('studio')
+    source = json_data.get('source')
+    runtime = json_data.get('runtime')
+    outline = json_data.get('outline')
+    label = json_data.get('label')
+    series = json_data.get('series')
+    year = json_data.get('year')
+
+    if json_data.get('cover_small') == None:
+        cover_small = ''
+    else:
+        cover_small = json_data.get('cover_small')
+
+    imagecut = json_data.get('imagecut')
+    tag = str(json_data.get('tag')).strip("[ ]").replace("'", '').replace(" ", '').split(',')  # 字符串转列表 @
+    actor = str(actor_list).strip("[ ]").replace("'", '').replace(" ", '')
+
+    if title == '' or number == '':
+        print('[-]Movie Data not found!')
+        #moveFailedFolder(filepath, conf.failed_folder()) 注释掉的内容
+        return
+
+    # if imagecut == '3':
+    #     DownloadFileWithFilename()
+
+    # ====================处理异常字符====================== #\/:*?"<>|
+    title = title.replace('\\', '')
+    title = title.replace('/', '')
+    title = title.replace(':', '')
+    title = title.replace('*', '')
+    title = title.replace('?', '')
+    title = title.replace('"', '')
+    title = title.replace('<', '')
+    title = title.replace('>', '')
+    title = title.replace('|', '')
+    release = release.replace('/', '-')
+    tmpArr = cover_small.split(',')
+    if len(tmpArr) > 0:
+        cover_small = tmpArr[0].strip('\"').strip('\'')
+
+    # ====================处理异常字符 END================== #\/:*?"<>|
+
+    # ===  替换Studio片假名
+    studio = studio.replace('アイエナジー', 'Energy')
+    studio = studio.replace('アイデアポケット', 'Idea Pocket')
+    studio = studio.replace('アキノリ', 'AKNR')
+    studio = studio.replace('アタッカーズ', 'Attackers')
+    studio = re.sub('アパッチ.*', 'Apache', studio)
+    studio = studio.replace('アマチュアインディーズ', 'SOD')
+    studio = studio.replace('アリスJAPAN', 'Alice Japan')
+    studio = studio.replace('オーロラプロジェクト・アネックス', 'Aurora Project Annex')
+    studio = studio.replace('クリスタル映像', 'Crystal 映像')
+    studio = studio.replace('グローリークエスト', 'Glory Quest')
+    studio = studio.replace('ダスッ！', 'DAS！')
+    studio = studio.replace('ディープス', 'DEEP’s')
+    studio = studio.replace('ドグマ', 'Dogma')
+    studio = studio.replace('プレステージ', 'PRESTIGE')
+    studio = studio.replace('ムーディーズ', 'MOODYZ')
+    studio = studio.replace('メディアステーション', '宇宙企画')
+    studio = studio.replace('ワンズファクトリー', 'WANZ FACTORY')
+    studio = studio.replace('エスワン ナンバーワンスタイル', 'S1')
+    studio = studio.replace('エスワンナンバーワンスタイル', 'S1')
+    studio = studio.replace('SODクリエイト', 'SOD')
+    studio = studio.replace('サディスティックヴィレッジ', 'SOD')
+    studio = studio.replace('V＆Rプロダクツ', 'V＆R PRODUCE')
+    studio = studio.replace('V＆RPRODUCE', 'V＆R PRODUCE')
+    studio = studio.replace('レアルワークス', 'Real Works')
+    studio = studio.replace('マックスエー', 'MAX-A')
+    studio = studio.replace('ピーターズMAX', 'PETERS MAX')
+    studio = studio.replace('プレミアム', 'PREMIUM')
+    studio = studio.replace('ナチュラルハイ', 'NATURAL HIGH')
+    studio = studio.replace('マキシング', 'MAXING')
+    studio = studio.replace('エムズビデオグループ', 'M’s Video Group')
+    studio = studio.replace('ミニマム', 'Minimum')
+    studio = studio.replace('ワープエンタテインメント', 'WAAP Entertainment')
+    studio = re.sub('.*/妄想族', '妄想族', studio)
+    studio = studio.replace('/', ' ')
+    # ===  替换Studio片假名 END
+
+    location_rule = eval(conf.location_rule())
+
+    if 'actor' in conf.location_rule() and len(actor) > 100:
+        print(conf.location_rule())
+        location_rule = eval(conf.location_rule().replace("actor", "'多人作品'"))
+    maxlen = conf.max_title_len()
+    if 'title' in conf.location_rule() and len(title) > maxlen:
+        shorttitle = title[0:maxlen]
+        location_rule = location_rule.replace(title, shorttitle)
+
+    # 返回处理后的json_data
+    json_data['title'] = title
+    json_data['actor'] = actor
+    json_data['release'] = release
+    json_data['cover_small'] = cover_small
+    json_data['tag'] = tag
+    json_data['location_rule'] = location_rule
+    json_data['year'] = year
+    json_data['actor_list'] = actor_list
+    if conf.is_transalte():
+        translate_values = conf.transalte_values().split(",")
+        for translate_value in translate_values:
+            json_data[translate_value] = translate(json_data[translate_value])
+    naming_rule = ""
+    for i in conf.naming_rule().split("+"):
+        if i not in json_data:
+            naming_rule += i.strip("'").strip('"')
+        else:
+            naming_rule += json_data.get(i)
+    json_data['naming_rule'] = naming_rule
+    return json_data
+
+
 def core_main_rename(file_path, number_th, conf: config.Config):
     # =======================================================================初始化所需变量
     multi_part = 0
@@ -77,11 +261,6 @@ def core_main_rename(file_path, number_th, conf: config.Config):
         return
 
     if json_data["number"] != number:
-        # fix issue #119
-        # the root cause is we normalize the search id
-        # print_files() will use the normalized id from website,
-        # but paste_file_to_folder() still use the input raw search id
-        # so the solution is: use the normalized search id
         number = json_data["number"]
     tag = json_data['tag']
     # =======================================================================判断-C,-CD后缀
